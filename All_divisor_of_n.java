8640d36b7e599065a2fe712dbbed1cbe39ec1b50
janak-ojha <janakrajojha66@gmail.com>
2024-01-31 00:09:59 +0530

    hello

--- All_divisor_of_n.java
+++ All_divisor_of_n.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class All_divisor_of_n {
+    //normal method
+    public static void dicisor(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+        if(n%i==0)
+        {
+            System.out.println(i);
+        }
+        }
+    }
+    public static void divde(int n)
+    {
+        int i;
+        for( i=1;i*i<n;i++)
+        {
+            if(n%i==0)
+            {
+                System.out.println(i);
+            }
+        }
+        for(int j=i;j>=1;j++)
+        {
+            if(n%j==0 || j==n/j)
+            {
+                System.out.println(j);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        dicisor(n);
+        divde(n);
+        
+    }
     
 }
