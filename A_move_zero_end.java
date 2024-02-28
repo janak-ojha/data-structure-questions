baad2e2f05e0acd4ba2184240aa80effe88d3a50
janak-ojha <janakrajojha66@gmail.com>
2024-02-28 17:48:51 +0530

    hello

---
+++ A_move_zero_end.java
@@ -0,0 +1,38 @@
+public class A_move_zero_end {
+    static void moves(int arr[])
+    {
+        int nz=arr[0];
+        int n=arr[0];
+        while (nz<arr.length) {
+            if(arr[nz] != 0)
+            {
+                int temp=arr[nz];
+                arr[nz]=arr[n];
+                arr[n]=temp;
+                nz++;
+                n++;
+            }
+            else{
+                nz++;
+            }
+            
+        }
+    }
+   
+    public static void main(String[] args) {
+        int arr[]={1,0,2,0,3,0,0};
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        moves(arr);
+        System.out.println();
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+        
+    }
+    
+}
