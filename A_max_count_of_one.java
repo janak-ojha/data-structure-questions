872bb9345aff26fe1c07fd4cac5dcd0c03b25365
janak-ojha <janakrajojha66@gmail.com>
2024-03-09 22:49:55 +0530

    hello

--- A_max_count_of_one.java
+++ A_max_count_of_one.java
@@ -25,9 +25,33 @@ public class A_max_count_of_one {
         }
         return maxconut;
     }
+    static int max_Cou(int arr[])
+    {  
+        int maxcou=0;
+        int counts=0;
+        for(int i=0;i<arr.length;i++)
+        {   
+            if(arr[i]==0)
+            {
+                counts=0;
+
+            }
+            else{
+                counts++;
+                if(counts>maxcou)
+                {
+                    maxcou=counts;
+                }
+            }
+           
+        }
+        return maxcou;
+        
+    }
     public static void main(String[] args) {
         int arr[]={0,1,1,0,1,0,1,1,1,1,0};
         System.out.println(max_co(arr));
+        System.out.println(max_Cou(arr));
         
     }
     
