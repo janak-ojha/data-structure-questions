373f36dddc9111be410b526f695b248f859f15b9
janak-ojha <janakrajojha66@gmail.com>
2024-04-19 02:37:10 +0530

    hello

---
+++ Bubble_sort.java
@@ -0,0 +1,39 @@
+public class Bubble_sort {
+    static void bubbl(int arr[])
+    {
+        int n= arr.length;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[]={22,33,55,23,77,99};
+        System.out.println("array before sorting");
+        printArr(arr);
+        System.out.println("after sorting using bubbe sort");
+        bubbl(arr);
+        printArr(arr);
+
+
+        
+    }
+    static void printArr(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    
+}
--- eg.java
+++ eg.java
@@ -1,24 +1,6 @@
 public class eg{
-    static int binary_search(int arr[],int key,int low,int high)
-    {
-        if(low <= high)
-        {
-            int mid = (low+high)/1;
-        
-        if(arr[mid]==key)
-        {
-            return mid;
-        }
-        else if(arr[mid]<key)
-        {
-            return binary_search(arr, key, mid+1, high);
-        }
-        else{
-            return binary_search(arr, key, low, mid-1);
-        }
-    }
-    return -1;
-}
+    
+  
    
  public static void main(String[] args) {
     int arr[]={1,2,3,4,9,11,15};
