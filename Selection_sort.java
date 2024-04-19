208b2b63996e596ac47c6aa6a9939a941bc9e738
janak-ojha <janakrajojha66@gmail.com>
2024-04-20 00:13:39 +0530

    hello

---
+++ Selection_sort.java
@@ -0,0 +1,40 @@
+public class Selection_sort {
+    static void sele(int arr[])
+    {
+        int n=arr.length;
+        
+        for(int i=0;i<n-1;i++)
+        {
+           int miIndex=i;
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[j]<arr[miIndex]){
+                    miIndex=j;
+
+                }
+
+            }
+            int temp=arr[miIndex];
+            arr[miIndex]=arr[i];
+            arr[i]=temp;
+
+        }
+    }
+    public static void main(String[] args) {
+      int arr[]={2,4,3,7,5};
+      System.out.println("array before sort");
+      printe(arr);
+      System.out.println("after sorting");
+      sele(arr);
+      printe(arr);
+    }
+    static void printe(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    
+}
